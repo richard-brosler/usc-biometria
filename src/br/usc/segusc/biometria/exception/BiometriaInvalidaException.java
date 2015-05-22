a7c64e22d50efadf2a2f829563cf78432cd8af46
richard-brosler <richard.brosler@gmail.com>
2015-05-21 23:58:00 -0300

    Ajustes finais

--- src/br/usc/segusc/biometria/exception/BiometriaInvalidaException.java
+++ src/br/usc/segusc/biometria/exception/BiometriaInvalidaException.java
@@ -1,5 +1,10 @@
 package br.usc.segusc.biometria.exception;
 
 public class BiometriaInvalidaException extends Exception {
-
+	public BiometriaInvalidaException(String msg) {
+		super(msg);
+	}
+	public BiometriaInvalidaException() {
+		super("Biometria Inválida!");
+	}
 }
--- src/br/usc/segusc/biometria/impl/Biometria.java
+++ src/br/usc/segusc/biometria/impl/Biometria.java
@@ -1,14 +1,66 @@
 package br.usc.segusc.biometria.impl;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import br.usc.segusc.biometria.exception.BiometriaInvalidaException;
 import br.usc.segusc.biometria.service.IBiometriaService;
+import br.usc.segusc.log.entity.LogLevel;
+import br.usc.segusc.log.impl.Log;
+import br.usc.segusc.log.services.LogFactory;
 
 public class Biometria implements IBiometriaService {
+	private class Biometrias{
+		private int [][] biometria;
+		private String login;
+		public Biometrias(String log, int [][] bio){
+			biometria = bio;
+			login = log;
+		}
+	}
+	private ArrayList<Biometrias> listaBio;
+	private int [][] bioms;
+	
+	public Biometria() {
+		listaBio = new ArrayList<Biometria.Biometrias>();
+		bioms = new int[4][4];
 
+		bioms[0][0] = 0; bioms[0][1] = 0; bioms[0][2] = 0; bioms[0][3] = 0;
+		bioms[1][0] = 1; bioms[1][1] = 1; bioms[1][2] = 1; bioms[1][3] = 1;
+		bioms[2][0] = 0; bioms[2][1] = 0; bioms[2][2] = 0; bioms[2][3] = 0;
+		bioms[3][0] = 1; bioms[3][1] = 1; bioms[3][2] = 1; bioms[3][3] = 1;
+		
+		listaBio.add(new Biometrias("Richard", bioms));
+		Random rnd = new Random(System.currentTimeMillis());
+		for (int i=0;i<10;i++){
+			bioms = new int[4][4];
+			for (int j=0;j<4;j++){
+				for (int k=0;k<4;k++){
+					bioms[j][k]=rnd.nextInt(1);
+				}
+			}
+			listaBio.add(new Biometrias("Richard", bioms));
+		}
+	}
 	@Override
 	public String scanear(int[][] matriz) throws BiometriaInvalidaException {
-		// TODO Auto-generated method stub
-		return null;
+		Boolean valido=true;
+		for (Biometrias biometrias : listaBio) {
+			for(int i=0;i<4;i++){
+				for(int j=0;j<4;j++){
+					if (biometrias.biometria[i][j] != matriz[i][j]){
+						valido = false;
+						break;
+					}
+				}
+				if (!valido) break;
+			}
+			if (!valido) 
+				break;
+			else
+				return biometrias.login;
+		}
+		throw new BiometriaInvalidaException();
 	}
 
 }
